a18f145097011fb3fc1f57ef6a1b002f13ff6ed7
Mahdi Ahmadi <mah.ahmadi360@gmail.com>
2023-02-18 21:17:01 +0330

    init

---
+++ Algorithms/Implementation/ClimbingTheLeaderboard.cs
@@ -0,0 +1,87 @@
+﻿namespace Algorithms;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var g = climbingLeaderboard(new List<int> { 100, 100, 50, 40, 40, 20, 10 },
+            new List<int> { 5, 25, 50, 120 });
+
+        Assert.Equal("6,4,2,1", string.Join(",", g));
+    }
+
+    public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+    {
+        var result = new List<int>();
+        /*var rank = 1;
+        var j = 0;
+        for (var i = player.Count - 1; i >= 0; i--)
+        {
+            for (j =j ; j < ranked.Count; j++)
+            {
+                if (ranked[j] <= player[i])
+                    break;
+
+                if (j == 0 ||
+                     ranked[j - 1] != ranked[j])
+                    rank++;
+            }
+            result.Insert(0, rank);
+        }*/
+        //ranked.Add(0);
+
+        //var rankCount = ranked.Count;
+        //var rankIndex = 0;
+        //var rank = 1;
+        //var scoreIndex = player.Count - 1;
+        //while (rankIndex < rankCount)
+        //{
+        //    if (player[scoreIndex] >= ranked[rankIndex])
+        //    {
+        //        result.Insert(0, rank);
+        //        scoreIndex--;
+
+        //        if (scoreIndex == -1)
+        //            break;
+        //        continue;
+        //    }
+
+        //    if (rankIndex == 0 ||
+        //        ranked[rankIndex] != ranked[rankIndex - 1])
+        //        rank++;
+
+        //    rankIndex++;
+        //}
+
+        ranked.Add(0);
+
+        var rankCount = ranked.Count;
+        var rankIndex = 0;
+        var rank = 1;
+        var scoreIndex = player.Count - 1;
+        while (rankIndex < rankCount)
+        {
+            if (rankIndex != 0 &&
+                ranked[rankIndex] == ranked[rankIndex - 1])
+            {
+                rankIndex++;
+                continue;
+            }
+
+            if (player[scoreIndex] >= ranked[rankIndex])
+            {
+                result.Insert(0, rank);
+                scoreIndex--;
+
+                if (scoreIndex == -1)
+                    break;
+                continue;
+            }
+
+            rankIndex++;
+            rank++;
+        }
+        return result;
+    }
+}
---
+++ Algorithms/Implementation/formingMagicSquare.cs
@@ -0,0 +1,102 @@
+﻿namespace TestProject1;
+
+public class formingMagicSquareTest
+{
+    [Fact]
+    public void Test1()
+    {
+        var g = formingMagicSquare(new List<List<int>> { new List<int>{5, 3 ,4 },
+                new List<int>{ 1, 5, 8 },
+                new List<int>{   6, 4 ,2}});
+        Assert.Equal(7, g);
+    }
+
+    [Fact]
+    public void Test12()
+    {
+        var g = formingMagicSquare(new List<List<int>>
+        {
+            new List<int>{4 ,9 ,2 },
+            new List<int>{3 ,5 ,7},
+            new List<int>{8 ,1 ,5}
+        });
+
+        Assert.Equal(1, g);
+    }
+
+    [Fact]
+    public void Test13()
+    {
+        var g = formingMagicSquare(new List<List<int>>{
+            new List<int>{6,7,2},
+            new List<int>{1,5,9},
+            new List<int>{8,3,4}
+        });
+
+        Assert.Equal(0, g);
+    }
+
+    public int formingMagicSquare(List<List<int>> s)
+    {
+        int cost = int.MaxValue;
+        for (int i = 0; i < 4; i++)
+        {
+            var rotatedCost = GetCost(s);
+            if (rotatedCost < cost)
+                cost = rotatedCost;
+
+            s = Flip(s);
+            rotatedCost = GetCost(s);
+            if (rotatedCost < cost)
+                cost = rotatedCost;
+
+            if (i == 3)
+                break;
+            s = Rotate(s);
+        }
+        return cost;
+    }
+
+    private List<List<int>> Rotate(List<List<int>> s)
+    {
+        var rotated = new List<List<int>>();
+        for (int i = 0; i < 3; i++)
+        {
+            var row = new List<int>{
+                s[2][i],
+                s[1][i],
+                s[0][i]
+            };
+            rotated.Add(row);
+        }
+        return rotated;
+    }
+
+    private List<List<int>> Flip(List<List<int>> s)
+    {
+        var rotated = new List<List<int>>();
+        for (int i = 0; i < 3; i++)
+        {
+            var row = new List<int>{
+                s[2][i],
+                s[1][i],
+                s[0][i]
+            };
+            rotated.Add(row);
+        }
+        return rotated;
+    }
+
+    private int GetCost(List<List<int>> s)
+    {
+        var magicSquare = new int[3, 3]{
+            {8,3,4},{1,5,9},{6,7,2}
+        };
+        var cost = 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                cost += Math.Abs(s[i][j] - magicSquare[i, j]);
+
+        return cost;
+    }
+}
