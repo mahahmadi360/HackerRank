936f81ba57bf86e4aef540a224ed17a1261dd1cb
Mahdi Ahmadi <mah.ahmadi360@gmail.com>
2023-02-20 00:20:42 +0330

    plus minus

---
+++ Algorithms.Library/Algorithms/Implementation/PlusMinus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementation
+{
+    public static class PlusMinus
+    {
+        public static List<string> Calc(List<int> arr)
+        {
+            var positives = 0d;
+            var negatives = 0d;
+            var zros = 0d;
+            int i = 0;
+            for (i = 0; i < arr.Count; i++)
+            {
+                var val = arr[i];
+                if (val > 0)
+                    positives++;
+                else if (val < 0)
+                    negatives++;
+                else
+                    zros++;
+            }
+
+            positives = positives / i;
+            negatives = negatives / i;
+            zros = zros / i;
+
+            return new List<string>
+            {
+                positives.ToString("f6"),
+                negatives.ToString("f6"),
+                zros.ToString("f6")
+            };
+
+        }
+    }
+}
---
+++ Algorithms/Implementation/PlusMinusTest.cs
@@ -0,0 +1,40 @@
+﻿using Algorithms.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsTest.Implementation
+{
+    public class PlusMinusTest
+    {
+        public PlusMinusTest()
+        {
+            var culture =(CultureInfo) System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            culture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            var result = PlusMinus.Calc(new List<int> { 1, 1, 0, -1, -1 });
+
+            Assert.Equal("0.400000", result[0]);
+            Assert.Equal("0.400000", result[1]);
+            Assert.Equal("0.200000", result[2]);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var result = PlusMinus.Calc(new List<int> { -4, 3, -9, 0, 4, 1 });
+
+            Assert.Equal("0.500000", result[0]);
+            Assert.Equal("0.333333", result[1]);
+            Assert.Equal("0.166667", result[2]);
+        }
+    }
+}
