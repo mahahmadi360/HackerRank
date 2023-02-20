e9b4272f29dd1d842fde7e3e44067224541b93bb
Mahdi Ahmadi <mah.ahmadi360@gmail.com>
2023-02-20 23:47:41 +0330

    time conversation

---
+++ Algorithms.Library/Algorithms/Implementation/TimeConversion.cs
@@ -0,0 +1,17 @@
+﻿namespace Algorithms.Implementation
+{
+    public static class TimeConversion
+    {
+        public static string timeConversion(string s)
+        {
+            var hour = int.Parse(s.Substring(0, 2));
+            var minuetes = s.Substring(2, 6);
+            var timeFormat = s.Substring(8, 2);
+            if (timeFormat == "PM")
+                hour += 12;
+            if (hour == 24 || hour == 12)
+                hour -= 12;
+            return $"{hour:d2}{minuetes}";
+        }
+    }
+}
---
+++ Algorithms/Implementation/TimeConversionTest.cs
@@ -0,0 +1,14 @@
+﻿using Algorithms.Implementation;
+
+namespace AlgorithmsTest.Implementation
+{
+    public class TimeConversionTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var result = TimeConversion.timeConversion("07:05:45PM");
+            Assert.Equal("19:05:45", result);
+        }
+    }
+}
