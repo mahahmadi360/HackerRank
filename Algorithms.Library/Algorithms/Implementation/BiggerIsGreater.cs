37e63a5d3d08771af3c1c39b01cf9424a21bff60
Mahdi Ahmadi <mah.ahmadi360@gmail.com>
2023-02-22 01:07:40 +0330

    bigger is greater

---
+++ Algorithms.Library/Algorithms/Implementation/BiggerIsGreater.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementation
+{
+    public static class BiggerIsGreater
+    {
+        public static string biggerIsGreater(string word)
+        {
+            int i;
+            var w = word.ToArray();
+            for (i = w.Length - 1; i > 0; i--)
+            {
+                if (w[i].CompareTo(w[i - 1]) > 0)
+                    break;
+            }
+
+            if (i == 0)
+                return "no answer";
+
+            var replacement = w[i - 1];
+
+            var replaceValueInd = i;
+            for (int j = i; j < w.Length; j++)
+            {
+                if (w[j].CompareTo(w[replaceValueInd]) < 0
+                    && w[j].CompareTo(replacement) > 0)
+                    replaceValueInd = j;
+            }
+
+            w[i - 1] = w[replaceValueInd];
+            w[replaceValueInd] = replacement;
+
+            Array.Sort(w, i, w.Length - i, new Comparer());
+            //w = w.Take(i).Concat(w.Skip(i).Order()).ToArray();
+            return string.Join("", w);
+        }
+        public class Comparer : IComparer<char>
+        {
+            public int Compare(char x, char y)
+            {
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
---
+++ Algorithms/Implementation/BiggerIsGreaterTest.cs
@@ -0,0 +1,22 @@
+﻿using Algorithms.Implementation;
+
+namespace AlgorithmsTest.Implementation
+{
+    public class BiggerIsGreaterTest
+    {
+        [Theory]
+        [InlineData("l", "no answer")]
+        [InlineData("lmno", "lmon")]
+        [InlineData("dcba", "no answer")]
+        [InlineData("dcbb", "no answer")]
+        [InlineData("abdc", "acbd")]
+        [InlineData("abcd", "abdc")]
+        [InlineData("fedcbabcd", "fedcbabdc")]
+        public void Test(string testCase, string expectedResult)
+        {
+            var result = BiggerIsGreater.biggerIsGreater(testCase);
+
+            Assert.Equal(expectedResult,result );
+        }
+    }
+}
