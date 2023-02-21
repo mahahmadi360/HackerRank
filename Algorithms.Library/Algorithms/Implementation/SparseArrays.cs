b6a2d00840fef4c967cb60f8495a0b7b7c03c2c6
Mahdi Ahmadi <mah.ahmadi360@gmail.com>
2023-02-21 19:01:16 +0330

    sparse array

---
+++ Algorithms.Library/Algorithms/Implementation/SparseArrays.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementation
+{
+    public static class SparseArrays
+    {
+        public static List<int> matchingStrings(List<string> strings, List<string> queries)
+        {
+            /*strings.Sort();
+            var queryAndIndex = queries.Select((a, i) => new { Value = a, Index = i });
+
+            var querySorted = queryAndIndex.OrderBy(a => a.Value).ToList();
+            var queryIndex = 0;
+
+            var result = new int[queries.Count];
+            int stringIndex = 0;
+            while (stringIndex < strings.Count)
+            {
+                if (queryIndex == queries.Count)
+                    break;
+
+                var currentString = strings[stringIndex];
+                if (currentString == querySorted[queryIndex].Value)
+                    result[querySorted[queryIndex].Index] += 1;
+
+                if (currentString.CompareTo(querySorted[queryIndex].Value) == 1)
+                {
+                    queryIndex++;
+                    continue;
+                }
+                stringIndex++;
+            }
+
+            return result.ToList();*/
+            /*
+            var queryDic = queries.ToDictionary(a=> a, a => 0);
+            for (int i = 0; i < strings.Count; i++)
+            {
+                if (queryDic.ContainsKey(strings[i]))
+                {
+                    queryDic[strings[i]] += 1;
+                }
+            }
+
+            return queryDic.Select(a => a.Value).ToList();*/
+
+            List<int> res = new List<int>();
+            int count = 0;
+            for (int i = 0; i < queries.Count; i++)
+            {
+                count = 0;
+                foreach (string s in strings)
+                {
+                    if (queries[i] == s)
+                    {
+                        count++;
+                    }
+                }
+                res.Add(count);
+            }
+            return res;
+        }
+
+    }
+    }
+}
---
+++ Algorithms/Implementation/SparseArraysTest.cs
@@ -0,0 +1,55 @@
+﻿using Algorithms.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsTest.Implementation
+{
+    public class SparseArraysTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var inputs = new List<string> { "3", "def", "de", "fgh", "3" };
+            var queries = new List<string> { "de", "lmn", "fgh" };
+
+            var result = SparseArrays.matchingStrings(inputs, queries);
+            var expectedResult = new List<int> { 1, 0, 1 };
+
+            Assert.Equal(string.Join(",", expectedResult), string.Join(",", result));
+        }
+        [Fact]
+        public void Test2()
+        {
+            var inputs = new List<string> { "13",
+                "abcde",
+                "sdaklfj",
+                "asdjf",
+                "na",
+                "basdn",
+                "sdaklfj",
+                "asdjf",
+                "na",
+                "asdjf",
+                "na",
+                "basdn",
+                "sdaklfj",
+                "asdjf",
+                "5" };
+            var queries = new List<string> {"abcde",
+                "sdaklfj",
+                "asdjf",
+                "na",
+                "basdn" };
+
+            var result = SparseArrays.matchingStrings(inputs, queries);
+            var expectedResult = new List<int> {1,3,4,3,2};
+
+            Assert.Equal(string.Join(",", expectedResult), string.Join(",", result));
+
+        }
+
+    }
+}
